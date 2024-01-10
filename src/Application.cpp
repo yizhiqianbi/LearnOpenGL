5d47b11af092ac06db296b9aa358febae5f5cdca
yizhiqianbi <850587960@qq.com>
2024-01-10 15:38:15 +0800

    索引缓冲区
    glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,nullptr)；

--- src/Application.cpp
+++ src/Application.cpp
@@ -128,20 +128,34 @@ int main(void)
     /* Loop until the user closes the window */
 
     float positions[] = {
-    -0.5f, -0.5f,
-    0.0f, 0.5f,
-    0.5f, -0.5f
+    -0.5f,  -0.5f,  //0
+     0.5f,  -0.5f,  //1
+     0.5f,   0.5f,  //2
+    -0.5f,   0.5f   //3
     };
 
+    unsigned int indices[] = {
+        0,1,2,
+        2,3,0
+    };
+
+
+    //创建顶点缓冲区
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 6 * 2 *  sizeof(float), positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,sizeof(float) * 2 ,0) ;
 
+    // 创建索引缓冲区
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
+    // 创建ShaderProgram
     ShaderProgramSource source;
     source = ParseShader("resource/shaders/Basic.shade");
 
@@ -156,8 +170,11 @@ int main(void)
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        glDrawArrays(GL_TRIANGLES,0,3);
+        //glDrawArrays(GL_TRIANGLES,0,3);
+        // 
         //glDrawElements();
+        // glDrawElement画得索引
+        glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,nullptr);
         
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
